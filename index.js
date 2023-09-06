3ad0c49cab244eaea01d880156799dbb0f9451c8
Chase <chasegarrett@tutanota.com>
2023-09-06 16:57:29 -0500

    complete addEmployee function

--- index.js
+++ index.js
@@ -95,6 +95,56 @@ const addRole = async () => {
     await viewAllRoles();
 };
 
+// get roles function
+const getRoles = async () => {
+    const res = await query(`SELECT id, title FROM roles;`);
+
+    for (const roles of res) {
+        const role = {};
+        role.id = roles.id;
+        role.name = roles.title;
+        prompts.roleArr.push(role);
+    }
+};
+
+// get employees function
+const getEmployees = async () => {
+    const res = await query(`SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employees;`);
+
+    for (const employees of res) {
+        const employee = {};
+        employee.id = employees.id;
+        employee.name = employees.name;
+        prompts.employeeArr.push(employee);
+    }
+};
+
+// add employee function
+const addEmployee = async () => {
+    // get roles for choices
+    await getRoles();
+    // get employees for manager choices
+    await getEmployees();
+
+    const newEmployee = await inquirer.prompt(prompts.addEmployee);
+
+    // get role id
+    const role = await query(`SELECT id FROM roles WHERE title = ?`, newEmployee.employeeRole);
+    const roleId = role[0].id;
+
+    // get manager id
+    const managerName = newEmployee.employeeManager.split(' ');
+    const manager = await query(`SELECT id FROM employees WHERE first_name = (?) AND last_name = (?)`,
+    [managerName[0], managerName[1]]);
+    const managerId = manager[0].id;
+
+    // insert new employee
+    await query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+    [newEmployee.firstName.trim(), newEmployee.lastName.trim(), roleId, managerId]);
+
+    await viewAllEmployees();
+};
+
 // create menu with inquirer
 const menu = () => {
     inquirer.prompt(prompts.mainMenu)
@@ -115,10 +165,10 @@ const menu = () => {
             case 'Add a role':
                 addRole();
                 break;
-            case 'Add rmployee':
+            case 'Add an employee':
                 addEmployee();
                 break;
-            case 'Update rmployee role':
+            case 'Update an employee role':
                 updateEmployeeRole();
                 break;
             case 'Quit':
