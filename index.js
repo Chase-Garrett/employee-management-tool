dac19e1c57f951640ebd03c026285cd75d9a56a8
Chase <chasegarrett@tutanota.com>
2023-09-06 14:31:50 -0500

    refactor for add/update functions ease of use

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const inquirer = require('inquirer');
 // import db connection
 const db = require('./db/connection').mysql();
 
+// import prompts
+const prompts = require('./lib/prompts');
+
 //view all departments function
 const viewAllDepartments = () => {
     const sql = `SELECT * FROM departments`;
@@ -54,94 +57,9 @@ const viewAllEmployees = () => {
     });
 };
 
-//add department function
-const addDepartment = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'department',
-            message: 'What is the name of the department?'
-        }
-    ])
-    .then((answer) => {
-        const sql = `INSERT INTO departments (name) VALUES (?)`;
-        const params = [answer.department];
-        db.query(sql, params, (err, result) => {
-            if(err) {
-                console.log(err);
-                return;
-            }
-            console.log('Department added successfully!');
-            menu();
-        });
-    });
-};
-
-// get departments function
-const getDepartments = () => {
-    const sql = `SELECT name FROM departments`;
-    const departments = db.query(sql);
-    // set up array to hold department names
-    const departmentArray = [];
-    
-    return departmentArray;
-};
-
-// add role function
-const addRole = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'role',
-            message: 'What is the name of the role?'
-        },
-        {
-            type: 'input',
-            name: 'salary',
-            message: 'What is the salary for this role?'
-        },
-        {
-            type: 'list',
-            name: 'department',
-            message: 'What department does this role belong to?',
-            // list of departments from db
-            choices: getDepartments()
-        }
-     ])
-        .then((answer) => {
-            const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-            const params = [answer.role, answer.salary, answer.department];
-            db.query(sql, params, (err, result) => {
-                if(err) {
-                    console.log(err);
-                    return;
-                }
-                console.log('Role added successfully!');
-                menu();
-            });
-        }
-    );
-};
-
 // create menu with inquirer
 const menu = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'menu',
-            message: 'What would you like to do?',
-            choices: [
-                'View All Departments',
-                'View All Roles',
-                'View All Employees',
-                'Add Department',
-                'Add Role',
-                'Add Employee',
-                'Update Employee Role',
-                'Quit'
-            ]
-        }
-    ])
+    inquirer.prompt(prompts.mainMenu)
     .then((answer) => {
         switch(answer.menu) {
             case 'View All Departments':
